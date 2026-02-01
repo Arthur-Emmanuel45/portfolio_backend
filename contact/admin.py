d334a5683183e27868ccbe70a5673c7a3188a2c3
Arthur-Emmanuel45 <arthuremmanuel45@gmail.com>
2026-02-01 07:37:54 +0000

    Fix migrations automatically on Render deploy

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import ContactMessage
 
-@admin.register(ContactMessage)
-class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'created_at')
-    search_fields = ('email', 'name')
+admin.site.register(ContactMessage)
